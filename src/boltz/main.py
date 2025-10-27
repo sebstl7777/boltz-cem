378f0443dcaf16642d02d300b824074c3cb101ef
Slash715 <130778900+Slash715@users.noreply.github.com>
2025-10-27 15:30:10 -0400

    change to cpu for debugging
    Comment out accelerator and devices parameters, set to CPU and 1 device.

--- src/boltz/main.py
+++ src/boltz/main.py
@@ -1333,8 +1333,10 @@ def predict(  # noqa: C901, PLR0915, PLR0912
         default_root_dir=out_dir,
         strategy=strategy,
         callbacks=[pred_writer],
-        accelerator=accelerator,
-        devices=devices,
+        #accelerator=accelerator,
+        #devices=devices,
+        accelerator = "cpu"
+        devices = 1
         precision=32 if model == "boltz1" else "bf16-mixed",
     )
 
